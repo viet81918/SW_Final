dccb0af8e32e34646ead383619a1fcebde7f664d
thienlcde170520 <thienle2105@gmail.com>
2024-06-11 12:24:01 +0700

    Login+SignUP+LoginGG

--- src/main/java/Common/Constants.java
+++ src/main/java/Common/Constants.java
@@ -9,10 +9,10 @@ package Common;
  * @author ASUS
  */
 public class Constants {
-        public static String GOOGLE_CLIENT_ID = "861336407219-9pau27upj8qhchdlsr9ljsr630ojfb2h.apps.googleusercontent.com";
-	public static String GOOGLE_CLIENT_SECRET = "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";
-	public static String GOOGLE_REDIRECT_URI = "http://localhost:9999/FPTeam/loginGG";
-	public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
-	public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
-	public static String GOOGLE_GRANT_TYPE = "authorization_code";
+        public static final String GOOGLE_CLIENT_ID = "861336407219-9pau27upj8qhchdlsr9ljsr630ojfb2h.apps.googleusercontent.com";
+	public static final String GOOGLE_CLIENT_SECRET = "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";
+	public static final String GOOGLE_REDIRECT_URI = "http://localhost:9999/FPTeam/loginGG";
+	public static final String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
+	public static final String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
+	public static final String GOOGLE_GRANT_TYPE = "authorization_code";
 }
