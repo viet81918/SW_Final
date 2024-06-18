6743d001228327dd1bd6c9002a351ea5cc915ab6
viet81918 <viet81918@gmail.com>
2024-06-18 13:15:02 +0700

    fix Home and Update Review

--- src/main/java/Model/Publishers.java
+++ src/main/java/Model/Publishers.java
@@ -19,7 +19,7 @@ public class Publishers extends Users{
     private int Money;
     private String RegistrationDate;
 
-    public Publishers(String id, String name, String gmail, String password, int role, String bank_account, int profit, String description, String AvatarLink, int Money, String RegistrationDate) {
+   public Publishers(String id, String name, String password, String gmail,String bank_account, int profit, String description, String AvatarLink, int Money,int role,String RegistrationDate) {
         super(id, name, gmail, password, role);
         this.bank_account = bank_account;
         this.profit = profit;
@@ -28,7 +28,6 @@ public class Publishers extends Users{
         this.Money = Money;
         this.RegistrationDate = RegistrationDate;
     }
-
     public String getBank_account() {
         return bank_account;
     }
